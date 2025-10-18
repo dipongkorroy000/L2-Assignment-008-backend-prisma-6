7e5dec7fa853620a527137b830780c3787dd0b3c
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-10-18 22:12:44 +0600

    schedule tasks completed

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { ScheduleService } from "./schedule.service";
+import pick from "../../helper/pick";
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
@@ -17,20 +18,27 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
 });
 
 const schedulesForDoctor = catchAsync(async (req: Request, res: Response) => {
+  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const filters = pick(req.query, ["startDateTime", "endDateTime"]);
+
+  const result = await ScheduleService.schedulesForDoctor(filters, options);
+
   sendResponse(res, {
-    status: 201,
+    status: 200,
     success: true,
-    message: "Schedule created successfully!",
-    data: "",
+    message: "Schedule retrieved successfully!",
+    data: result,
   });
 });
 
 const deleteScheduleFromDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await ScheduleService.deleteScheduleFromDB(req.params.id as string);
+
   sendResponse(res, {
-    status: 201,
+    status: 200,
     success: true,
-    message: "Schedule created successfully!",
-    data: "",
+    message: "Schedule deleted successfully!",
+    data: result,
   });
 });
 
--- src/app/modules/schedule/schedule.service.ts
+++ src/app/modules/schedule/schedule.service.ts
@@ -1,5 +1,7 @@
 import { addMinutes, addHours, format } from "date-fns";
 import { prisma } from "../../shared/prisma";
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
+import { Prisma } from "@prisma/client";
 
 const insertIntoDB = async (payload: any) => {
   const { startTime, endTime, startDate, endDate } = payload;
@@ -49,9 +51,34 @@ const insertIntoDB = async (payload: any) => {
   return schedules;
 };
 
-const schedulesForDoctor = async () => {};
+const schedulesForDoctor = async (filters: any, options: IOptions) => {
+  const { page, limit, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
 
-const deleteScheduleFromDB = async (id: string) => {};
+  const { startDateTime: filterStartDateTime, endDateTime: filterEndDateTime } = filters;
+
+  const andConditions: Prisma.ScheduleWhereInput[] = [];
+
+  if (filterStartDateTime && filterEndDateTime) {
+    andConditions.push({ AND: [{ startDateTime: { gte: filterStartDateTime } }, { endDateTime: { lte: filterEndDateTime } }] });
+  }
+
+  const whereConditions: Prisma.ScheduleWhereInput = andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.schedule.findMany({
+    where: whereConditions,
+    skip: (page - 1) * limit,
+    take: limit,
+    orderBy: { [sortBy]: sortOrder },
+  });
+
+  const total = await prisma.schedule.count({ where: whereConditions });
+
+  return { meta: { page, limit, total }, data: result };
+};
+
+const deleteScheduleFromDB = async (id: string) => {
+  return await prisma.schedule.delete({ where: { id } });
+};
 
 export const ScheduleService = {
   insertIntoDB,
